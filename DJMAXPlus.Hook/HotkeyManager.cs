3c328f836be4ac0c2326b91c1f35942a30c8bd5b
ruby3141 <ruby3141@gmail.com>
2023-12-12 17:59:25 +0900

    add .Hook.HotkeyManager
    asdff
    
    asdfasdf

---
+++ DJMAXPlus.Hook/HotkeyManager.cs
@@ -0,0 +1,32 @@
+﻿using DJMAXPlus.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Win32;
+
+namespace DJMAXPlus.Hook
+{
+    public class HotkeyManager
+    {
+        public ControllerStates ControllerState { get; private set; } = ControllerStates.Uninitialized;
+
+        CancellationTokenSource tokenSource;
+        Thread? workerThread;
+
+        public HotkeyManager()
+        {
+            tokenSource = new CancellationTokenSource();
+        }
+
+        public bool RegisterHotKey(HotKey hotKey)
+        {
+            return true;
+        }
+
+
+
+
+    }
+}
