d259934ab4ff28b9471a6c5ba1864b612e8aa161
ruby3141 <ruby3141@gmail.com>
2023-12-21 08:46:22 +0900

    add ApplicationHelper

---
+++ DJMAXPlus.MainForm/ApplicationHelper.cs
@@ -0,0 +1,67 @@
+﻿namespace DJMAXPlus.MainForm
+{
+    public static class ApplicationHelper
+    {
+        private static object _applicationThreadLock = new object();
+        private static Thread? applicationThread;
+        private static Control? invokerControl;
+
+        /// <summary>
+        /// Make new Thread and run Application Main Loop on there.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Thrown when Application main loop is already running</exception>
+        public static void Run(ApplicationContext applicationContext)
+        {
+            lock (_applicationThreadLock)
+            {
+                if (applicationThread != null)
+                    throw new InvalidOperationException("Application already running");
+
+                applicationThread = new Thread((ctx) => { invokerControl = new Control(); invokerControl.CreateControl(); Application.Run((ApplicationContext)ctx!); });
+                applicationThread.Start(applicationContext);
+            }
+        }
+
+
+
+        /// <summary>
+        /// Make Application Exit() and join Main Loop Thread
+        /// </summary>
+        public static void Stop()
+        {
+            lock (_applicationThreadLock)
+            {
+                if (applicationThread != null)
+                {
+                    Application.Exit();
+                    applicationThread.Join();
+
+                    applicationThread = null;
+                }
+            }
+        }
+
+        public static object Invoke(Delegate method, params object?[]? args)
+        {
+            return invokerControl.Invoke(method, args);
+        }
+
+        public static void Invoke(Action method) => Invoke(method, null);
+
+        public static object Invoke(Delegate method) => Invoke(method, null);
+
+        public static T Invoke<T>(Func<T> method) => (T)Invoke(method, null);
+
+        public static IAsyncResult BeginInvoke(Delegate method, params object?[]? args)
+        {
+            if (invokerControl is null)
+                return Task.FromException(new NullReferenceException("invokerControl is not generated."));
+
+            return invokerControl.BeginInvoke(method, args);
+        }
+
+        public static IAsyncResult BeginInvoke(Action method) => BeginInvoke(method, null);
+
+        public static IAsyncResult BeginInvoke(Delegate method) => BeginInvoke(method, null);
+    }
+}
--- DJMAXPlus/Program.cs
+++ DJMAXPlus/Program.cs
@@ -1,1 +1,12 @@
-﻿// Nothing in here.
+﻿using System.Windows.Forms;
+using DJMAXPlus.MainForm;
+
+Console.WriteLine($"From Thread {Thread.CurrentThread.ManagedThreadId}");
+var f = new Form();
+ApplicationContext applicationContext = new();
+ApplicationHelper.Run(applicationContext);
+Thread.Sleep(3000);
+ApplicationHelper.Invoke(() => Console.WriteLine($"From Thread {Thread.CurrentThread.ManagedThreadId}"));
+Thread.Sleep(3000);
+Console.WriteLine($"From Thread {Thread.CurrentThread.ManagedThreadId}");
+ApplicationHelper.Stop();
