b773165a194480680246d4a2b3473d2e8a17310f
ruby3141 <ruby3141@gmail.com>
2023-12-21 11:44:54 +0900

    add MainForm
    It has almost no contents. Just test logic.

---
+++ DJMAXPlus.MainForm/MainForm.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace DJMAXPlus.MainForm
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            hotKeyController = new HotKeyController();
+            SuspendLayout();
+            // 
+            // hotKeyController
+            // 
+            hotKeyController.Location = new Point(0, 0);
+            hotKeyController.Name = "hotKeyController";
+            hotKeyController.Size = new Size(0, 0);
+            hotKeyController.TabIndex = 0;
+            hotKeyController.HotKeyDown += hotKeyController_HotKeyDown;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(hotKeyController);
+            Name = "MainForm";
+            Text = "MainForm";
+            Load += MainForm_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private HotKeyController hotKeyController;
+    }
+}
---
+++ DJMAXPlus.MainForm/MainForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DJMAXPlus.MainForm
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            hotKeyController.RegisterHotKey(new HotKey
+            {
+                Modifier = HotKey.Modifiers.Mod_Ctrl | HotKey.Modifiers.Mod_Shift,
+                VKeyCode = HotKey.VKeyCodes.VK_F12,
+            });
+        }
+
+        private void hotKeyController_HotKeyDown(HotKeyController sender, HotKey hotKey)
+        {
+            Console.WriteLine($"HotKey Down!: {hotKey.Modifier}, {hotKey.VKeyCode}");
+        }
+    }
+}
