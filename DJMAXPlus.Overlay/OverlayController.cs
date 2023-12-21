bc08ff8c88caf3473dd192fa367a5c43b69021bb
ruby3141 <ruby3141@gmail.com>
2023-12-21 17:46:46 +0900

    temp

--- DJMAXPlus.Overlay/OverlayController.cs
+++ DJMAXPlus.Overlay/OverlayController.cs
@@ -1,7 +1,18 @@
-﻿namespace DJMAXPlus.Overlay
+﻿using CefSharp;
+using CefSharp.OffScreen;
+
+namespace DJMAXPlus.Overlay
 {
     public class OverlayController
     {
+        private ChromiumWebBrowser browser;
+
+        public OverlayController()
+        {
+            browser = new ChromiumWebBrowser();
+        }
+
+
 
 
     }
--- DJMAXPlus/Program.cs
+++ DJMAXPlus/Program.cs
@@ -1,12 +1,7 @@
 ﻿using System.Windows.Forms;
 using DJMAXPlus.MainForm;
 
-Console.WriteLine($"From Thread {Thread.CurrentThread.ManagedThreadId}");
-var f = new Form();
-ApplicationContext applicationContext = new();
+MainForm m = new();
+ApplicationContext applicationContext = new(m);
 ApplicationHelper.Run(applicationContext);
-Thread.Sleep(3000);
-ApplicationHelper.Invoke(() => Console.WriteLine($"From Thread {Thread.CurrentThread.ManagedThreadId}"));
-Thread.Sleep(3000);
-Console.WriteLine($"From Thread {Thread.CurrentThread.ManagedThreadId}");
-ApplicationHelper.Stop();
+ApplicationHelper.Join();
