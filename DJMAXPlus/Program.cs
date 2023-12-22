5d75862b9228eedb59cddfccbef2659e891c83f4
ruby3141 <ruby3141@gmail.com>
2023-12-22 18:01:11 +0900

    [temp] test codes

--- DJMAXPlus/Program.cs
+++ DJMAXPlus/Program.cs
@@ -1,7 +1,21 @@
 ﻿using System.Windows.Forms;
+using CefSharp;
+using CefSharp.OffScreen;
 using DJMAXPlus.MainForm;
+using DJMAXPlus.Overlay;
 
+Cef.Initialize(new CefSettings() { LogSeverity = LogSeverity.Info, });
+
+OverlayController oc = new();
 MainForm m = new();
+m.Resize += M_Resize;
+
+oc.LoadPage("https://page.devr.is/DJMAXPlus/lanecover.html");
 ApplicationContext applicationContext = new(m);
 ApplicationHelper.Run(applicationContext);
 ApplicationHelper.Join();
+
+void M_Resize(object? sender, EventArgs e)
+{
+    oc.SetViewportSize(m.Size);
+}
