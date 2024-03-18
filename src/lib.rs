ea0dbdd40fe355b6334084f86f1c037b759f6ec2
CDnX <ruby3141@gmail.com>
2024-03-19 00:09:16 +0900

    add overlay struct template
    by the way it was dx11, not dx12.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
-use hudhook::hooks::dx12::ImguiDx12Hooks;
+use hudhook::{hooks::dx11::ImguiDx11Hooks, ImguiRenderLoop};
+use imgui::Ui;
 
-hudhook::hudhook!(ImguiDx12Hooks, ()); // TODO
+struct DPOverlay;
+
+impl DPOverlay {
+    fn new() -> Self {
+        Self
+    }
+}
+
+impl ImguiRenderLoop for DPOverlay {
+    fn render(&mut self, ui: &mut Ui) {
+
+    }
+}
+
+// DLL entry point
+hudhook::hudhook!(ImguiDx11Hooks, DPOverlay::new());
