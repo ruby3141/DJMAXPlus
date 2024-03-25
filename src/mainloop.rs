ca983e296ddd26542caaefac5e1b0a32ecb1701e
ruby3141 <ruby3141@gmail.com>
2024-03-25 18:00:18 +0900

    add structure for Lanecover window

--- src/mainloop.rs
+++ src/mainloop.rs
@@ -1,17 +1,22 @@
-use crate::configs::DjmaxPlusConfig;
-use crate::helper;
+use crate::{common::LaneDirection::*, configs::DjmaxPlusConfig, helper};
 use std::{borrow::Borrow, collections::VecDeque, path::PathBuf};
 
 use hudhook::{util, ImguiRenderLoop};
 use imgui::{
-    sys::{igSetNextWindowPos, ImGuiCond, ImGuiCond_Once, ImVec2}, Ui
+    sys::{igSetNextWindowPos, ImGuiCond, ImGuiCond_Once, ImVec2},
+    StyleVar, Ui, WindowFlags,
 };
 
 pub struct DjmaxPlusMainLoop {
     config: DjmaxPlusConfig,
-    current_lanecover_index: u8,
+
+    viewport_width: f32,
+    viewport_height: f32,
+    current_lanecover_index: usize,
+
     is_noti_popup_opened: bool,
     notifications: VecDeque<String>,
+
     dll_path: Option<PathBuf>,
 }
 
@@ -19,6 +24,8 @@ impl DjmaxPlusMainLoop {
     pub fn new() -> Self {
         Self {
             config: DjmaxPlusConfig::new(),
+            viewport_width: 0.,
+            viewport_height: 0.,
             current_lanecover_index: 0,
             is_noti_popup_opened: false,
             notifications: VecDeque::new(),
@@ -64,9 +71,14 @@ impl ImguiRenderLoop for DjmaxPlusMainLoop {
         if self.is_noti_popup_opened {
             ctx.io_mut().mouse_draw_cursor = true;
         }
+
+        let [width, height] = ctx.main_viewport().size;
+        self.viewport_width = width;
+        self.viewport_height = height;
     }
 
     fn render(&mut self, ui: &mut Ui) {
+        // Notification popup
         if !self.is_noti_popup_opened && !self.notifications.is_empty() {
             self.is_noti_popup_opened = true;
             ui.open_popup("notification_popup")
@@ -89,5 +101,45 @@ impl ImguiRenderLoop for DjmaxPlusMainLoop {
                 ui.close_current_popup();
             }
         });
+
+        // Lanecover
+        let style_lanecover_windowbordersize = ui.push_style_var(StyleVar::WindowBorderSize(0.));
+        let style_lanecover_windowpadding = ui.push_style_var(StyleVar::WindowPadding([0., 0.]));
+        ui.window("lanecover")
+            .flags(WindowFlags::from_iter([
+                WindowFlags::NO_DOCKING,
+                WindowFlags::NO_DECORATION,
+                WindowFlags::NO_INPUTS,
+                WindowFlags::NO_SCROLL_WITH_MOUSE,
+                WindowFlags::NO_MOVE,
+            ]))
+            .position(
+                [
+                    match self.config.lane_direction {
+                        // To be filled
+                        Left => 0.,
+                        Center => 0.,
+                        Right => 0.,
+                    },
+                    0.,
+                ],
+                imgui::Condition::Always,
+            )
+            .size(
+                [
+                    (self.viewport_height * 4. / 9.).floor(), // 25% of viewport width, with forced 16:9 screen aspect ratio
+                    (self.viewport_height
+                        * self.config.lanecover_configs[self.current_lanecover_index].visible_ratio
+                            as f32
+                        / 1000.)
+                        .floor(),
+                ],
+                imgui::Condition::Always,
+            )
+            .build(|| {
+                todo!();
+            });
+        style_lanecover_windowpadding.pop();
+        style_lanecover_windowbordersize.pop();
     }
 }
