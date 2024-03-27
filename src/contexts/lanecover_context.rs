60e2a8561457f3e3426f9afee99d380a6b00d4be
ruby3141 <ruby3141@gmail.com>
2024-03-27 14:27:39 +0900

    make Lanecover follow visibility config

--- src/contexts/lanecover_context.rs
+++ src/contexts/lanecover_context.rs
@@ -18,41 +18,43 @@ impl LanecoverContext {
         runtime: &RuntimeContext,
         config: &DjmaxplusConfig,
     ) {
-        let style_lanecover_windowbordersize = ui.push_style_var(StyleVar::WindowBorderSize(0.));
-        let style_lanecover_windowpadding = ui.push_style_var(StyleVar::WindowPadding([0., 0.]));
-        ui.window("lanecover")
-            .flags(WindowFlags::from_iter([
-                WindowFlags::NO_DOCKING,
-                WindowFlags::NO_DECORATION,
-                WindowFlags::NO_INPUTS,
-                WindowFlags::NO_SCROLL_WITH_MOUSE,
-                WindowFlags::NO_MOVE,
-            ]))
-            .position(
-                [
-                    (runtime.viewport_effective_offset_h()
-                        + match &config.lane_direction {
-                            LaneDirection::Left => runtime.viewport_effective_width() * 0.041,
-                            LaneDirection::Center => runtime.viewport_effective_width() * 0.375,
-                            LaneDirection::Right => runtime.viewport_effective_width() * 0.709,
-                        })
-                    .floor(),
-                    0.,
-                ],
-                imgui::Condition::Always,
-            )
-            .size(
-                [
-                    runtime.viewport_effective_width() / 4., // 25% of viewport width, with forced 16:9 screen aspect ratio
-                    (runtime.viewport_height()
-                        * config.lanecover_configs[self.config_index].visible_ratio as f32
-                        / 1000.)
+        if config.lanecover_visible {
+            let style_lanecover_windowbordersize = ui.push_style_var(StyleVar::WindowBorderSize(0.));
+            let style_lanecover_windowpadding = ui.push_style_var(StyleVar::WindowPadding([0., 0.]));
+            ui.window("lanecover")
+                .flags(WindowFlags::from_iter([
+                    WindowFlags::NO_DOCKING,
+                    WindowFlags::NO_DECORATION,
+                    WindowFlags::NO_INPUTS,
+                    WindowFlags::NO_SCROLL_WITH_MOUSE,
+                    WindowFlags::NO_MOVE,
+                ]))
+                .position(
+                    [
+                        (runtime.viewport_effective_offset_h()
+                            + match &config.lane_direction {
+                                LaneDirection::Left => runtime.viewport_effective_width() * 0.041,
+                                LaneDirection::Center => runtime.viewport_effective_width() * 0.375,
+                                LaneDirection::Right => runtime.viewport_effective_width() * 0.709,
+                            })
                         .floor(),
-                ],
-                imgui::Condition::Always,
-            )
-            .build(|| {});
-        style_lanecover_windowpadding.pop();
-        style_lanecover_windowbordersize.pop();
+                        0.,
+                    ],
+                    imgui::Condition::Always,
+                )
+                .size(
+                    [
+                        runtime.viewport_effective_width() / 4., // 25% of viewport width, with forced 16:9 screen aspect ratio
+                        (runtime.viewport_height()
+                            * config.lanecover_configs[self.config_index].visible_ratio as f32
+                            / 1000.)
+                            .floor(),
+                    ],
+                    imgui::Condition::Always,
+                )
+                .build(|| {});
+            style_lanecover_windowpadding.pop();
+            style_lanecover_windowbordersize.pop();
+        }
     }
 }
